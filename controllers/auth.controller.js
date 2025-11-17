4f2ab94bb94f2b69400332fc5dec7bf70c585447
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-17 12:23:56 +0530

    feat(auth) implement complete authntication flow (signup, login, logout)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,11 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
 
 export const register = async (req, res) => {
     const { name, email, password, role } = req.body;
@@ -10,8 +15,8 @@ export const register = async (req, res) => {
         if (!alreadyExist) {
             const user = await User.create({ name, email, password, role });
             res.status(201).json({ message: "User created successfully", user })
-        }else{
-            res.status(400).json({message: "User already exist"})
+        } else {
+            res.status(400).json({ message: "User already exist" })
         }
 
     } catch (error) {
@@ -36,15 +41,14 @@ export const login = async (req, res) => {
             { expiresIn: "7d" }
         )
 
-        res.status(200).json({
-            message: "login succesfully",
-            token,
-            user: {
-                id: user._id,
-                email: user.email,
-                name: user.name
-            }
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "strict",
+            maxAge: 24 * 60 * 60 * 1000,
         })
+
+        res.status(200).json({ message: "Login successfully" })
 
     } catch (error) {
         res.status(404).json({ message: "error in login", error })
@@ -52,3 +56,12 @@ export const login = async (req, res) => {
     }
 }
 
+export const logout = async (req, res) => {
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+    })
+
+    res.status(200).json({message: "Logged out succesfully"});
+}
--- routes/auth.Routes.js
+++ routes/auth.Routes.js
@@ -1,11 +1,12 @@
 import express from "express"
-import { login, register } from "../controllers/auth.controller.js"
+import { login, logout, register } from "../controllers/auth.controller.js"
 
 
 const autRoutes = express.Router()
 
-autRoutes.post("/login", login)
+autRoutes.post("/login", login);
 autRoutes.post("/register", register);
+autRoutes.post("/logout", logout);
 
 
 export default autRoutes
