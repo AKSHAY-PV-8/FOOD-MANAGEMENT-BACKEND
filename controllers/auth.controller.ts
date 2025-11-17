177aaaba687816c17ad2441a38be072f61d78b5b
AKSHAY-PV-8 <akshaypv19@gmail.com>
2025-11-17 14:29:50 +0530

    feat(auth) update typescript

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,4 +1,5 @@
-import User from "../models/user.model.js";
+import {Request, Response} from "express"
+import User from "../models/user.model.ts";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
@@ -7,24 +8,24 @@ import dotenv from "dotenv"
 dotenv.config()
 
 
-export const register = async (req, res) => {
+export const register = async (req: Request, res: Response) => {
     const { name, email, password, role } = req.body;
 
     try {
         const alreadyExist = await User.findOne({ email })
         if (!alreadyExist) {
             const user = await User.create({ name, email, password, role });
-            res.status(201).json({ message: "User created successfully", user })
+            return res.status(201).json({ message: "User created successfully", user })
         } else {
-            res.status(400).json({ message: "User already exist" })
+            return res.status(400).json({ message: "User already exist" })
         }
 
     } catch (error) {
-        res.status(400).json({ message: "error in register", error })
+        return res.status(400).json({ message: "error in register", error })
     }
 }
 
-export const login = async (req, res) => {
+export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     try {
@@ -37,7 +38,7 @@ export const login = async (req, res) => {
 
         const token = jwt.sign(
             { userId: user._id, email: user.email },
-            process.env.JWT_SECRET,
+            process.env.JWT_SECRET!,
             { expiresIn: "7d" }
         )
 
@@ -56,7 +57,7 @@ export const login = async (req, res) => {
     }
 }
 
-export const logout = async (req, res) => {
+export const logout = async (req: Request, res: Response) => {
     res.clearCookie("token", {
         httpOnly: true,
         secure: true,
@@ -65,3 +66,4 @@ export const logout = async (req, res) => {
 
     res.status(200).json({message: "Logged out succesfully"});
 }
+
